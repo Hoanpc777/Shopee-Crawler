80d82e04220383487b06c9dbbbcd7c8871696cb3
Phan Công Hoàn <hoan.pc@citigo.id>
2023-03-31 11:58:36 +0700

    update

--- func/character_process.py
+++ func/character_process.py
@@ -1,12 +1,13 @@
-import json
 import pandas as pd
-import requests
 import re
 
 # Get data from dict value
-def flat_dict(data, columns):
-    raw = pd.json_normalize(data[0])[[columns]].set_index([pd.Index(['lv1','lv2','lv3'])]).T.reset_index().drop('index',axis=1)
-    return raw
+def flat_dict(data):
+    df = pd.DataFrame(data)
+    unnest = df.explode('categories')
+    list_cate = [i['display_name'] for i in unnest['categories']]
+    cate_df = pd.concat([data,pd.DataFrame(list_cate).T], axis=1)
+    return cate_df
 
 def remove_char_in_Square_brackets(text):
   result = re.sub(r'\[[^]]*\]', '', text).strip()
--- func/data_process.py
+++ func/data_process.py
@@ -1,33 +1,57 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import random
 from seleniumwire import webdriver as wiredriver
 from seleniumwire.utils import decode
 import json
-import pandas as pd
-import requests
-import numpy as np
-import datetime, time
-import os
+# Chrome
+from selenium.webdriver.chrome.service import Service as chrome_Service
+from selenium.webdriver.chrome.options import Options as chrome_Options
+#Firefox
+from selenium.webdriver.firefox.service import Service as ff_Service
+from selenium.webdriver.firefox.options import Options as ff_Options
+#Edge
+from selenium.webdriver.edge.service import Service as edge_Service
+from selenium.webdriver.edge.options import Options as edge_Options
 
-# Run mô phỏng chrome
+# Run browser
 def browser():
-  chrome_options = Options()
-  chrome_options.add_argument('--headless') # run Chrome in headless mode (without GUI)
-  service = Service('/path/to/chromedriver') # path to chromedriver executable
-  driver = wiredriver.Chrome(service=service, options=chrome_options)
+    # # Random to choose browser
+    # """
+    # 1-Firefox 2-Chrome 3-Edge
+    # """
+    ran = random.randint(1, 3)
+    if ran == 1: driver = firefox_browser()
+    elif ran == 2: driver = chrome_browser()
+    elif ran == 3: driver = edge_browser()
+    # driver = firefox_browser()
+    return driver
+    
+# Run firefox
+def firefox_browser():
+    options = ff_Options()
+    options.add_argument('--headless') # run in headless mode (without GUI)
+    service = ff_Service('/path/to/geckodriver',log_path='nul') # path to chromedriver executable
+    driver = wiredriver.Firefox(service=service, options=options)
+    return driver
 
-  # driver = webdriver.Firefox()#webdriver.Chrome(options=chrome_options)
-  return driver
+# Run chrome
+def chrome_browser():
+    options = chrome_Options()
+    options.add_argument('--headless') # run in headless mode (without GUI)
+    service = chrome_Service('/path/to/chromedriver') # path to chromedriver executable
+    driver = wiredriver.Chrome(service=service, options=options)
+    return driver
 
+# run edge browser
+def edge_browser():
+    options = edge_Options()
+    options.add_argument('--headless') # run in headless mode (without GUI)
+    service = edge_Service('/path/to/msedgedriver') # path to chromedriver executable
+    driver = wiredriver.Edge(service=service, options=options)
+    return driver
+
+# run json data
 def get_json_data(url, api):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless') # run Chrome in headless mode (without GUI)
-    service = Service('/path/to/chromedriver') # path to chromedriver executable
-    driver = wiredriver.Chrome(service=service, options=chrome_options)
+    driver = browser()
     driver.get(url)
     for request in driver.requests:
         if request.response:
