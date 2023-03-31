497429a496fe5ee33cf60393823055541e35df47
Phan Công Hoàn <hoan.pc@citigo.id>
2023-03-31 11:57:58 +0700

    update

--- func/shopee_crawler.py
+++ func/shopee_crawler.py
@@ -1,22 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from seleniumwire import webdriver as wiredriver
 from seleniumwire.utils import decode
 import json
 import pandas as pd
 import requests
-import numpy as np
 import datetime, time
-import os
 from .data_process import browser, get_json_data
 from .character_process import flat_dict as flat_dict
+from func.var.config import header_config
 from typing import List
 
-# Lấy thông tin sản phẩm theo từ khóa tìm kiếm
+# Lấy thông tin sản phẩm theo từ khóa tìm kiếm theo số trang cần lấy
 def search_get_items(url = f'https://shopee.vn/search?keyword=b%E1%BB%89m%20youli',
                      api = f'https://shopee.vn/api/v4/search/search_items?by=',
                      page_number = 1):
@@ -27,6 +19,7 @@ def search_get_items(url = f'https://shopee.vn/search?keyword=b%E1%BB%89m%20youl
             # url = f'https://shopee.vn/search?keyword=s%E1%BB%AFa%20r%E1%BB%ADa%20m%E1%BA%B7t%20nuskin&page={i}'
             url = f'{url}&page={i}'
             driver = browser()
+            time.sleep(1)
             driver.get(url)
             for request in driver.requests:
                 if request.response:
@@ -35,14 +28,47 @@ def search_get_items(url = f'https://shopee.vn/search?keyword=b%E1%BB%89m%20youl
                         body = decode(response.body, response.headers.get('Content-Encoding', 'Identify'))
                         decoded_body = body.decode('utf8')
                         json_data = json.loads(decoded_body)
-                        r_list_search = pd.concat(pd.json_normalize(i['item_basic']) for i in json_data['items'])
+                        r_list_search = pd.json_normalize(i['item_basic'] for i in json_data['items'])
                         r_list_search['page_number'] = i
                         list_search = pd.concat([list_search,r_list_search],axis=0)
             driver.close()
+            time.sleep(1)
         except:
             pass
     list_search = list_search.reset_index().drop('index',axis=1)
     # driver.close()
+    return list_search
+
+# Lấy thông tin sản phẩm theo từ khóa tìm kiếm theo page chỉ định cụ thể
+def search_get_items_with_selected_page(url = f'https://shopee.vn/search?keyword=b%E1%BB%89m%20youli',
+                                        api = f'https://shopee.vn/api/v4/search/search_items?by=',
+                                        page_number = 0):
+    # driver = browser()
+    list_search = pd.DataFrame()
+    try:
+        # url = f'https://shopee.vn/search?keyword=s%E1%BB%AFa%20r%E1%BB%ADa%20m%E1%BA%B7t%20nuskin&page={i}'
+        url = f'{url}&page={page_number}'
+        driver = browser()
+        time.sleep(1)
+        driver.get(url)
+        # time.sleep(1)
+        for request in driver.requests:
+            if request.response:
+                if request.url.startswith(api):
+                    response = request.response
+                    body = decode(response.body, response.headers.get('Content-Encoding', 'Identify'))
+                    decoded_body = body.decode('utf8')
+                    json_data = json.loads(decoded_body)
+                    r_list_search = pd.json_normalize(i['item_basic'] for i in json_data['items'])
+                    r_list_search['page_number'] = page_number
+                    list_search = pd.concat([list_search,r_list_search],axis=0)
+        driver.close()
+        time.sleep(1)
+    except:
+        pass
+    list_search = list_search.reset_index().drop('index',axis=1)
+    # list_search = list_search.astype({'shopid':int,
+    #                                   'itemid':int})
     return list_search
 
 # Lấy danh sách sản phẩm phải có mã giảm giá
@@ -126,27 +152,64 @@ def get_items_from_industry (sub_facet: str, industry_list: List, page_number_to
     list_item_by_menu = list_item_by_menu.reset_index().drop('index',axis=1)
     return list_item_by_menu
 
-# Get detail infor items
+# Lấy giá từ modelid, shopid, itemid
+def get_item_price(df):
+    item_url = f"https://shopee.vn/api/v4/product/get_purchase_quantities_for_selected_model?itemId={df['itemid']}&modelId={df['modelid']}&shopId={df['shopid']}"
+
+    payload={
+    }
+    headers = header_config.headers
+    response = requests.request("GET", item_url, headers=headers, data=payload)
+
+    item_raw=response.json()
+    price_r_data = pd.json_normalize(item_raw['available_price_and_stocks'])
+    price_r_data = price_r_data[price_r_data.display_price >0]
+    min_price = price_r_data[price_r_data.display_price == price_r_data.display_price.min()]
+    price_data = pd.concat([pd.json_normalize(item_raw)[['price_before_discount']],min_price], axis = 1)
+    price_data = price_data[['price_before_discount','display_stock','display_price']]
+    return price_data
+
+
+# Thông tin cơ bản sản phẩm
 def get_in4_item(shopid: int, itemid: int):
-  item_url = f"https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}"
+    item_url = f"https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}"
 
-  payload={
-  }
-  headers = {
-    'authority': 'shopee.vn',
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-    'x-api-source': 'pc',
-    'x-kl-ajax-request': 'Ajax_Request',
-    'x-requested-with': 'XMLHttpRequest',
-    'x-shopee-language': 'vi'
-  }
+    payload={
+    }
+    headers = header_config.headers
+    response = requests.request("GET", item_url, headers=headers, data=payload)
+    item_raw=response.json()
+    # overview product infor
+    product_data=pd.json_normalize(item_raw['data'])
+    inf = product_data[['shopid','itemid','name','brand','categories','historical_sold','item_rating.rating_star','images']]#'price','stock','discount','historical_sold','sold','description','categories','shop_vouchers','global_sold'
+    inf = pd.concat([inf,flat_dict(inf['categories'])],axis=1).rename(columns={'name':'parent_name'}).drop('categories',axis=1)    
+    # Detail product infor
+    model_data = pd.json_normalize(item_raw['data']['models'])[['itemid','name','modelid']].assign(shopid=shopid)
+    get_price = pd.concat(model_data.apply(lambda x: get_item_price(x), axis=1).tolist(), ignore_index=True)
+    price_data = pd.concat([model_data,get_price],axis=1).rename(columns={'itemid':'item','shopid':'shop'})
+    # price_data = price_data.astype({'item':int,
+    #                                 'shop':int})
+    df = price_data.merge(inf, how='left', left_on=['item','shop'],right_on=['itemid','shopid'])
+    df = df[df['itemid'].notnull()].reset_index().drop('index',axis=1)
+    return df
 
-  response = requests.request("GET", item_url, headers=headers, data=payload)
+# Thông tin chi tiết sản phẩm
+def get_full_in4_item(shopid: int, itemid: int):
+    item_url = f"https://shopee.vn/api/v4/item/get?itemid={itemid}&shopid={shopid}"
 
-  item_raw=response.json()
-  product_data=pd.json_normalize(item_raw['data'])
-  inf = product_data[['shopid','itemid','brand','price','stock','discount','historical_sold','sold','description','item_rating.rating_star','images','categories','shop_vouchers','global_sold']]
-  inf = pd.concat([inf,flat_dict(inf['categories'],'display_name')],axis=1)
-  return inf
+    payload={
+    }
+    headers = header_config.headers
+    response = requests.request("GET", item_url, headers=headers, data=payload)
+    item_raw=response.json()
+    product_data=pd.json_normalize(item_raw['data'])
+    # product_data = product_data.astype({'itemid':int,
+    #                                     'shopid':int})
+    model_data = pd.json_normalize(item_raw['data']['models'])[['itemid','name','modelid']].assign(shopid=shopid)
+    get_price = pd.concat(model_data.apply(lambda x: get_item_price(x), axis=1).tolist(), ignore_index=True)
+    price_data = pd.concat([model_data,get_price],axis=1).rename(columns={'itemid':'item','shopid':'shop'})
+    # price_data = price_data.astype({'item':int,
+    #                                 'shop':int})
+    df = price_data.merge(product_data, how='left', left_on=['item','shop'],right_on=['itemid','shopid'])
+    df = df[df['itemid'].notnull()].reset_index().drop('index',axis=1)
+    return df
